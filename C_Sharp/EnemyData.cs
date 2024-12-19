7b2e0e49ce416f890f7f29a01a954ecf28c82161
l3miage-yaha <samyyaha28@gmail.com>
2024-12-19 11:25:54 +0100

    Update EnemyData.cs

--- C_Sharp/EnemyData.cs
+++ C_Sharp/EnemyData.cs
@@ -1,11 +1,10 @@
+namespace Projet_Survivor.C_Sharp;
+
 public class EnemyData
 {
     public string Name { get; set; }
-    
     public int Rectangle_X { get; set; }
-    
     public int Rectangle_Y { get; set; }
-    
     public int Size { get; set; }
     public string Type { get; set; }
     public int HP { get; set; }
